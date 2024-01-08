1515a48cbef714b162adada5ba732699d88e292d
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-08 18:52:48 +0530

    Update Code.cpp

--- January/01-Assign Cookies/Code.cpp
+++ January/01-Assign Cookies/Code.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    int findContentChildren(vector<int>& greed, vector<int>& cookieSize)
+    {
+        sort(greed.begin(), greed.end()); //O(nlogn)
+        sort(cookieSize.begin(), cookieSize.end()); //O(mlogm)
+        
+        int currentChild = 0;
+        int currentCookie = 0;
+        
+        while(currentChild < greed.size() && currentCookie < cookieSize.size())
+        {
+            if(greed[currentChild] <= cookieSize[currentCookie])
+                currentChild++;
+            
+            currentCookie++;
+        }
+        return currentChild;
+    }
+};
+
+/*
+size of greed -> n
+size of cookieSize -> m
+
+Time Complexity = O(nlogn + mlogm)
+Space Complexity = O(logn + logm)
+*/
