b51329bf100d0c73807c0279d04c54d566ae4bf3
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-12 01:59:29 +0530

    Update code.cpp

--- January/11-Maximum Difference Between Node and Ancestor/code.cpp
+++ January/11-Maximum Difference Between Node and Ancestor/code.cpp
@@ -11,7 +11,38 @@
  */
 class Solution {
 public:
-    int maxAncestorDiff(TreeNode* root) {
+    
+    // function to find ancestors
+    void findAncestor(TreeNode* root, TreeNode* parent, vector<int>current, 
+                      unordered_map<TreeNode*, vector<int>>&ancestors)
+    {
+        if(!root) return;
         
+        current.push_back(parent->val);
+        ancestors[root] = current;
+        
+        findAncestor(root->left,root,current,ancestors);
+        findAncestor(root->right,root,current,ancestors);
+    }
+    
+    int maxAncestorDiff(TreeNode* root) 
+    {
+        unordered_map<TreeNode*, vector<int>>ancestors;
+        vector<int>current;
+        
+        findAncestor(root->left,root,current,ancestors);
+        findAncestor(root->right,root,current,ancestors);     
+        
+        int maxDifference = INT_MIN;
+        for(auto current : ancestors)
+        {
+            TreeNode* currentNode = current.first;
+            for(auto ancestorNode : current.second)
+            {
+                int currentDifference = abs(currentNode->val - ancestorNode);
+                maxDifference = max(maxDifference, currentDifference);
+            }
+        }
+        return maxDifference;
     }
 };
