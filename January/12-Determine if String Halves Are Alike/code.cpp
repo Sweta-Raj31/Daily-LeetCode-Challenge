117b2aa84a9c788faf2be8075e76418685827713
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-12 23:49:27 +0530

    Update code.cpp

--- January/12-Determine if String Halves Are Alike/code.cpp
+++ January/12-Determine if String Halves Are Alike/code.cpp
@@ -1,1 +1,28 @@
-12-
+class Solution {
+public:
+
+bool isvowel(char x)
+{
+    if (x == 'a' || x == 'e' || x == 'i' || x == 'o'|| x == 'u' || x == 'A' || x == 'E' || x == 'I'|| x == 'O' || x == 'U')
+      return true;
+
+  return false;
+}
+    bool halvesAreAlike(string s) {
+        
+        int c1=0,c2=0;
+        for(int i=0;i<(s.size())/2;i++)
+        {
+           if(isvowel(s[i]))
+            c1++;
+        }
+        for(int i=(s.size())/2;i<s.size();i++)
+        {
+            if(isvowel(s[i]))
+            c2++;
+        }
+        cout<<c1<<" "<<c2<<endl;
+        return c1==c2;
+        
+    }
+};
