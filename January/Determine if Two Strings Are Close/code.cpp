23f668a0ab02004dfae0111f16ae8b37f92d1902
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-14 23:12:31 +0530

    Update code.cpp

--- January/Determine if Two Strings Are Close/code.cpp
+++ January/Determine if Two Strings Are Close/code.cpp
@@ -1,1 +1,36 @@
 
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) 
+    {
+        // case1: different len
+        if(word1.size() != word2.size()) return false;
+        
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        for(int index = 0; index < word1.size(); index++)
+        {
+            freq1[word1[index]-'a']++;
+            freq2[word2[index]-'a']++;
+        }
+        
+        // case 2 : different char
+        for(int current = 0; current < 26; current++)
+        {
+            if((freq1[current] == 0 && freq2[current] != 0) || 
+              (freq1[current] != 0 && freq2[current] == 0))
+                return  false;
+        }
+        
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        // case 3: freq distribution
+        for(int current = 0; current < 26; current++)
+        {
+            if(freq1[current] != freq2[current])
+                return false;
+        }
+        // case4 : close
+        return true;
+    }
+};
