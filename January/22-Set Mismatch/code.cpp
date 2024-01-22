4e8e93a01fd98d956ad0177376c2f614b71c9509
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-23 00:12:58 +0530

    Update code.cpp

--- January/22-Set Mismatch/code.cpp
+++ January/22-Set Mismatch/code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        map<int, int> mp;
+        vector<int> v;
+        int sum=0;
+        for (auto x : nums) {
+            mp[x]++;
+        }
+        for (auto x : mp) {
+           sum+=x.first;
+           if(x.second==2)
+            v.push_back(x.first);
+        }
+        int n=nums.size();
+        int total_sum=(n*(n+1))/2;
+        
+        cout << total_sum-sum << endl;
+        v.push_back(total_sum-sum);
+        return v;
+    }
+};
