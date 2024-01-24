7e65b8e47cb7eb255c1d7f2379944bc0657915e6
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-24 23:24:26 +0530

    Update code.cpp

--- January/24-Pseudo-Palindromic Paths in a Binary Tree/code.cpp
+++ January/24-Pseudo-Palindromic Paths in a Binary Tree/code.cpp
@@ -1,1 +1,55 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool isPalindrome(vector<int>&count)
+{
+    int oddFreq=0;
+    for(auto CurrentIndex:count)
+    {
+        if(CurrentIndex%2==1)
+        {
+            oddFreq++;
+        }
+    }
+    if(oddFreq>1)
+    {
+        return false;
+    }
+    return true;
+}
+void findPseudoPalindromicPaths(TreeNode *root, vector<int> &count,int &ans)
+{
+    if(root==NULL)return;
+    count[root->val]++;
+    findPseudoPalindromicPaths(root->left,count,ans);
+    findPseudoPalindromicPaths(root->right,count,ans);
+    if(root->left==NULL && root->right==NULL)
+    {
+        if(isPalindrome(count))
+        {
+            ans++;
+        }
+    }
+    count[root->val]--;
+    return;
+}
+    int pseudoPalindromicPaths (TreeNode* root) {
+        int ans=0;
+        vector<int>count(10,0);
+        findPseudoPalindromicPaths(root,count,ans);
+        return ans;
+    }
+};
+// Time Complexity =O(N)  
+//space Complexity =o(H)
