c006dbaa925f675131b91ba3707eb137588a9997
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-01-31 23:16:56 +0530

    Update code.cpp

--- January/31-Daily Temperatures/code.cpp
+++ January/31-Daily Temperatures/code.cpp
@@ -1,1 +1,45 @@
 
+//next grater, next smaller ,just prev grate,r just prev smaller using stack or we can say monotonic stack
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        vector<int>ans(temp.size(),0);
+      /*  for(int i=0;i<temp.size();i++)
+        {
+            for(int j=i+1;j<temp.size();j++)
+            {
+                if(temp[i]<temp[j])
+                {
+                    ans[i]=abs(j-i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    the brute force approach of n^2 complexity we will reduce it using stack   
+        */
+        stack<int>st;
+        
+        for(int index=temp.size()-1;index>=0;index--)
+        {
+          
+
+          while(!st.empty() && temp[st.top()]<=temp[index])
+          {
+               st.pop();
+          }
+          if(st.empty())
+          {
+              ans[index]=0;
+
+          }
+          else
+          {
+              ans[index]=st.top()-index;
+          }
+          st.push(index);
+
+        }
+        return ans;
+    }
+};
