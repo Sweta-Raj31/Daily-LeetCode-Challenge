c17ae32d45a6fc27190754d8ed27f90a6223f0c7
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-06 00:07:10 +0530

    Update code.cpp

--- February/05-Paatal Lok/code.cpp
+++ February/05-Paatal Lok/code.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        queue<char>q;
+        int ans=-1;
+        for(int i=0;i<s.size();i++)
+        if(q.empty())
+        {     ans=i;
+            q.push(s[i]);
+        }
+        else
+        {
+            if(s[i]==q.front())
+            {
+                q.pop();
+
+            }
+              
+        }
+        return ans;
+    }
+};
