3710e6e318e69cfc8d9249a5f84905a110d29af2
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-06 21:06:10 +0530

    Update code.cpp

--- February/06-Group Anagrams/code.cpp
+++ February/06-Group Anagrams/code.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        for(auto k:strs){
+            string a=k;
+            sort(k.begin(),k.end());
+            mp[k].push_back(a);
+        }
+        for(auto& k:mp){
+            ans.push_back(k.second);
+        }
+        return ans;
+    }
+};
