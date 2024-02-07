10b5c0a6b90325b751cb14d6d4116a856c9231e7
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-07 22:04:37 +0530

    Update code.cpp

--- February/07-Sort Characters By Frequency/code.cpp
+++ February/07-Sort Characters By Frequency/code.cpp
@@ -1,1 +1,28 @@
-0
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>freq;
+        for(auto current:s)
+        {
+            freq[current]++;
+        }
+        priority_queue<pair<int,char>>pq;
+        for(auto current:freq)
+        {
+            pq.push({current.second,current.first});
+        }
+        string ans="";
+        while(!pq.empty())
+        {
+            int count=pq.top().first;
+            int currentchar=pq.top().second;
+            pq.pop();
+            while(count--)
+            {
+                ans+=currentchar;
+            }
+        }
+        return ans;
+        
+    }
+};
