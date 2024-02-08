43a95c6ef055fc432d4a9d0ba099ae7cd631004c
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-09 00:09:46 +0530

    Update code.cpp

--- February/08-Perfect Square/code.cpp
+++ February/08-Perfect Square/code.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+int numsSquareUsingRecursion(int n,vector<int>&memo)
+{
+        if(n<4) return n;
+        if(memo[n]!=-1) return memo[n];
+       int ans=INT_MAX;
+       for (int i = 1; i * i <= n; i++) {
+		int square = i * i;
+		ans = min(ans, 1 + numsSquareUsingRecursion(n - square,memo));
+        
+	}
+        return memo[n]=ans;
+}
+    int numSquares(int n) {
+        vector<int>memo(n+1,-1);
+        int ans=numsSquareUsingRecursion( n,memo);
+        return ans;
+    }
+};
