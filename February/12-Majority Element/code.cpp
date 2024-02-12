e3c5971429a1a110b78755aa48c18d7daf68c3c9
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-12 21:52:49 +0530

    Update code.cpp

--- February/12-Majority Element/code.cpp
+++ February/12-Majority Element/code.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> a;
+        for(int i:nums)
+            a[i]++;
+        int maxval=nums[0];
+        for(auto x:a)
+        {
+            if(x.second>a[maxval])
+                maxval=x.first;
+        }
+        return maxval;
+    }
+};
