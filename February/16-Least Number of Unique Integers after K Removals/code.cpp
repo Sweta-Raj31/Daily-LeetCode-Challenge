572c4bb3fdd915d9448028eaa6520a23ff426145
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-16 22:18:27 +0530

    Update code.cpp

--- February/16-Least Number of Unique Integers after K Removals/code.cpp
+++ February/16-Least Number of Unique Integers after K Removals/code.cpp
@@ -1,1 +1,26 @@
 
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int,int>mp;
+        for(auto x:arr)
+        mp[x]++;
+     int ans=0;
+      vector<int>freq;
+      for(auto x:mp)  
+      freq.push_back(x.second);
+      sort(freq.begin(),freq.end());
+      for(int i=0;i<freq.size();i++)
+      {
+          if(k>=freq[i])
+          {
+             k=k-freq[i] ;
+             freq[i]=-1;
+          }
+          if(freq[i]!=-1)
+           ans++;
+      }
+      return ans;
+        
+    }
+};
