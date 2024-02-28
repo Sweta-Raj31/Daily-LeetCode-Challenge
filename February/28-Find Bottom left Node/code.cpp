f67ce70401cc2dec493d56ddaa1c9c457c7cc19c
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-02-28 23:27:04 +0530

    Update code.cpp

--- February/28-Find Bottom left Node/code.cpp
+++ February/28-Find Bottom left Node/code.cpp
@@ -1,1 +1,39 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) 
+    {
+        TreeNode* leftMostNode = NULL;
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int len = q.size();
+            leftMostNode = q.front();
+            
+            while(len--)
+            {
+                TreeNode* currentNode = q.front();
+                q.pop();
+                
+                if(currentNode->left)
+                    q.push(currentNode->left);
+                if(currentNode->right)
+                    q.push(currentNode->right);
+            }
+        }
+        return leftMostNode->val;
+    }
+};
