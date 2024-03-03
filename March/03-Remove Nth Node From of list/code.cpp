bf296361a1457860b0f9d0b976a76f2fb0a17ef8
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-03-03 23:00:47 +0530

    Update code.cpp

--- March/03-Remove Nth Node From of list/code.cpp
+++ March/03-Remove Nth Node From of list/code.cpp
@@ -1,1 +1,50 @@
-M
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+int get_length(ListNode* &head)
+{
+    ListNode *temp=head;
+    int len=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        len++;
+    }
+    return len;
+}
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int i=1;
+        ListNode  *prev=head;
+        int len=get_length(head);
+        cout<<len;
+        if(len==1)
+        {
+            return NULL;
+        }
+        if(len==n)
+        {
+           return head->next; 
+        }
+        while(i<len-n)
+        {
+            prev=prev->next;
+            i++;
+        }
+        ListNode  *curr=prev->next;
+      
+        prev->next=curr->next;
+        
+
+        return head;
+    }
+};
