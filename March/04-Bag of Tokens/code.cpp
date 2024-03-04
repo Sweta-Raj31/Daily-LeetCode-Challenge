4335ff2dc397b7031b81effbbfe7697921991a37
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-03-04 14:50:44 +0530

    Update code.cpp

--- March/04-Bag of Tokens/code.cpp
+++ March/04-Bag of Tokens/code.cpp
@@ -1,1 +1,41 @@
 
+//2 pointers and greedy approach
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+        int left=0;
+        int right=tokens.size()-1;
+        int score=0;
+        int maxScore=0;
+        while(left<=right) // we cannot use = because for single element we cannot perform both operation
+        {
+            if(power>=tokens[left])
+            {
+               //face up 
+               cout<<tokens[left]<<endl;
+               power-=tokens[left] ;
+               left++;
+               score++;
+               maxScore=max(score,maxScore);
+            }
+           else if(score>0)
+            {
+                //face down 
+                cout<<"face down"<<tokens[right]<<endl;
+                power+=tokens [right];
+                right--;
+                score--;
+            }
+            else
+
+            {
+                break;
+            }
+        }
+        return maxScore;
+
+        
+    }
+};
