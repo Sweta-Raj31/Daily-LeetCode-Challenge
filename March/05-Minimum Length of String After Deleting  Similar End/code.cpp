14bffc2edc57b48976dc0adce71bcfc4e7bc5ca2
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-03-05 23:52:21 +0530

    Update code.cpp

--- March/05-Minimum Length of String After Deleting  Similar End/code.cpp
+++ March/05-Minimum Length of String After Deleting  Similar End/code.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i=0;
+        int j=s.size()-1;
+        auto ch=0;
+        while(i<j && s[i]==s[j])
+        {
+            ch=s[i];
+            while(i<j && s[i]==ch)
+            ++i;
+            while(i<j && s[j]==ch)
+            --j;
+        }
+        if(s[i]==ch) 
+        {
+            return 0;
+        }
+        return j-i+1;
+    }
+};
