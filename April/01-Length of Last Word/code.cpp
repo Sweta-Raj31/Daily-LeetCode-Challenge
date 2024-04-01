e0124258995ddfa2b602dc0b638f8970b17dfe70
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-01 21:51:16 +0530

    Update code.cpp

--- April/01-Length of Last Word/code.cpp
+++ April/01-Length of Last Word/code.cpp
@@ -1,1 +1,21 @@
 
+Length of Last Word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length()-1;
+        int index=0;
+        for(int i=n;i>=0;i--)
+        {
+            if(s[i]!=' ')
+            {
+                index++;
+            }
+            else if(index>0)
+            {
+                return index;
+            }
+        }
+        return index;
+    }
+};
