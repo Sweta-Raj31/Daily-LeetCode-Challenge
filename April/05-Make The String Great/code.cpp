539490bcd6fef39d2e093ec079e19fc972c20f3b
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-05 23:32:46 +0530

    Update code.cpp

--- April/05-Make The String Great/code.cpp
+++ April/05-Make The String Great/code.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    string makeGood(string s) 
+    {
+        stack<char>container;
+        
+        for(auto current : s)
+        {
+            // they are the same charater but in different cases
+            if(!container.empty() && abs(current-container.top()) == 32)
+                container.pop();
+            else
+                container.push(current);
+        }
+        int n = container.size();
+        string ans(n,'.'); // "Us"
+        int i = n-1;
+        while(!container.empty())
+        {
+            ans[i] = container.top();
+            container.pop();
+            i--;
+        }
+        return ans;
+    }
+};
+// B
+// b 
