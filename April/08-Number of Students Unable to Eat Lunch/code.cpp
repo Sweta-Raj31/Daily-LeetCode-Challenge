e0d2f4aec6df8fc2689ec099dd247894aa536b98
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-08 23:36:31 +0530

    Update code.cpp

--- April/08-Number of Students Unable to Eat Lunch/code.cpp
+++ April/08-Number of Students Unable to Eat Lunch/code.cpp
@@ -1,1 +1,37 @@
-
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int wantCircular=0;
+        int wantSquare=0;
+        for(auto x:students)
+        {
+            if(x==0)
+            wantCircular++;
+            else
+            wantSquare++;
+        }
+        for(auto x:sandwiches)
+        {
+            if(x==0)
+            {
+                if(wantCircular==0)
+                 return wantSquare;
+                 else
+                 {
+                    wantCircular--;
+                 }
+            }
+            else
+            {
+               if(wantSquare==0)
+                 return wantCircular;
+                 else
+                 {
+                    wantSquare--;
+                 } 
+            }
+        }
+        return 0;
+        
+    }
+};
