754fcae4232d1190c7a8c23695435c97b5c3cf16
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-12 23:44:08 +0530

    Update code.cpp

--- April/12-Tapping Rain water/code.cpp
+++ April/12-Tapping Rain water/code.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left=0;
+        int right=height.size()-1;
+        int leftmax=INT_MIN;
+        int rightmax=INT_MIN;
+        int ans=0;
+        while(left<right)
+        {
+            leftmax=max(leftmax,height[left]);
+            rightmax=max(rightmax,height[right]);
+            ans+=(leftmax<rightmax)?leftmax-height[left++]:rightmax-height[right--];
+        }
+        return ans;
 
+    }
+};
