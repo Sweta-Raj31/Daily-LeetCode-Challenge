b32aac63fc9998a41801a0c901e6a2eb7928ee2a
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-19 01:18:05 +0530

    Update code.cpp

--- April/18-Island Perimeter/code.cpp
+++ April/18-Island Perimeter/code.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int res=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    res += (i == 0 || grid[i-1][j] == 0 ? 1 : 0);
+                    res += (i == r-1 || grid[i+1][j] == 0 ? 1 : 0);
+                    res += (j == 0 || grid[i][j-1] == 0 ? 1 : 0);
+                    res += (j == c-1 || grid[i][j+1] == 0 ? 1 : 0);
+                }
+            }
+        }
+        return res;
+        
+    }
+};
