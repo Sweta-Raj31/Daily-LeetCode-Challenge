ab11a482128e84c910985918edacb1c3cad4a4c5
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-19 23:36:50 +0530

    Update code.cpp

--- April/19-Numbers of island/code.cpp
+++ April/19-Numbers of island/code.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+void dfs(int currentRow,int currentCol,vector<vector<char>>& grid)
+{
+    if(currentRow<0 || currentRow>=grid.size()||currentCol<0 ||currentCol>=grid[0].size() || grid[currentRow][currentCol]=='0')
+    return;
+    grid[currentRow][currentCol]='0';
+
+    dfs(currentRow+1,currentCol,grid);
+     dfs(currentRow-1,currentCol,grid);
+      dfs(currentRow,currentCol+1,grid);
+       dfs(currentRow,currentCol-1,grid);
+
+    return;
+}
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            { if(grid[i][j]=='1'){
+                dfs(i,j,grid);
+                ans++;}
+            }
+        }
+        return ans;
+        
+    }
+};
