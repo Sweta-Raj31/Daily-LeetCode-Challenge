a39b1314a4c5a50956ea70c7acf9f8975f0fa3c5
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-04-21 23:16:57 +0530

    Update code.cpp

--- April/22-Find if Path Exists in Graph/code.cpp
+++ April/22-Find if Path Exists in Graph/code.cpp
@@ -1,1 +1,36 @@
 
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>>graph(n);
+        //build the graph
+        for(int i=0;i<edges.size();i++)
+        {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        //bfs
+        queue<int>q;
+        vector<int>visited(n,0);
+        q.push(source);
+        visited[source]=1;
+        while(!q.empty())
+        {
+            int top=q.front();
+            q.pop();
+            if(top==destination)
+             return true;
+
+            for(int i=0;i<graph[top].size();i++)
+            {
+                if(visited[graph[top][i]]==0)
+                {
+                    q.push(graph[top][i]);
+                    visited[graph[top][i]] = 1;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
