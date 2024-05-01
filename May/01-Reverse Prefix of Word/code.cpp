00c438ede5a4e9cb8df129c82971a7622a34e858
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-01 22:06:54 +0530

    Update code.cpp

--- May/01-Reverse Prefix of Word/code.cpp
+++ May/01-Reverse Prefix of Word/code.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int j=0;
+        for(int i=0;i<word.size();i++)
+        {
+            if(word[i]==ch)
+            {
+                j=i;
+                break;
+            }
+        }
+        //cout<<j;
+        string s;
+        for(int i=0;i<=j;i++)
+        {
+            s.push_back(word[i]);
+        }
+       
+        
+        reverse(s.begin(),s.end());
+        // cout<<s;
+        for(int i=j+1;i<=word.size()-1;i++)
+        {
+           s.push_back(word[i]);
+        }
+        return s;
+        
+    }
+};
