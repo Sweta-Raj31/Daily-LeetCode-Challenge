72e0ac3f3bb56ed248e8a69e89640ec60a29b281
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-04 07:53:05 +0530

    Update code.cpp

--- May/04-Boats to Save People/code.cpp
+++ May/04-Boats to Save People/code.cpp
@@ -1,1 +1,26 @@
 
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int boatCount=0;
+        int left=0;
+        int right=people.size()-1;
+        while(left<=right)
+        {
+            if(people[left]+people[right]<=limit)
+            {
+                left++;
+                right--;
+
+            }
+            else
+            {
+                right--;
+            }
+            boatCount++;
+        }
+        return boatCount;
+        
+    }
+};
