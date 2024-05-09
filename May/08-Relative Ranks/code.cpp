958593573635b3f851dd4c3a8f26e8f274e4c8bd
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-09 21:03:45 +0530

    Update code.cpp

--- May/08-Relative Ranks/code.cpp
+++ May/08-Relative Ranks/code.cpp
@@ -1,1 +1,35 @@
 
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int len=score.size();
+        vector<string>ans(len," ");
+        priority_queue<pair<int,int>>pq; //elemment:index
+        for(int i=0;i<len;i++)
+        {
+          pq.push({score[i],i});
+        }
+        int rank=1;
+        while(!pq.empty())
+        {
+            int currentIndex=pq.top().second;
+            pq.pop();
+           if(rank == 1)
+                ans[currentIndex] = "Gold Medal";
+            
+            else if(rank == 2)
+                ans[currentIndex] = "Silver Medal";
+            
+            else if(rank == 3)
+                ans[currentIndex] = "Bronze Medal";
+            
+            else
+                ans[currentIndex] = to_string(rank);
+
+                rank++;
+
+        }
+  return ans;
+        
+    }
+};
