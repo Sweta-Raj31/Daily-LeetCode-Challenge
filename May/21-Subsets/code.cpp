07b629579d927d02a5951037cc86a67f75b9b4ce
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-21 22:22:18 +0530

    Update code.cpp

--- May/21-Subsets/code.cpp
+++ May/21-Subsets/code.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+void generateAllSubset(int currentIndex,vector<int>&currentAns,vector<vector<int>>&ans,vector<int>& nums ){
+
+    if(currentIndex==nums.size())
+    {
+       ans.push_back(currentAns);
+       return;
+    }
+    // take
+    currentAns.push_back(nums[currentIndex]);
+    generateAllSubset(currentIndex+1,currentAns,ans,nums);
+
+    //undo
+    currentAns.pop_back();
+
+
+    //not take
+    generateAllSubset(currentIndex+1,currentAns,ans,nums);
+}
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>currentAns;
+        generateAllSubset(0,currentAns,ans,nums);
+        return ans;
+        
+    }
+};
