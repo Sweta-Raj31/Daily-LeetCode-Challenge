524de353344a0aa8e6d979f34063316ca2700edc
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-22 23:48:57 +0530

    Update code.cpp

--- May/22-Palindrome Partitioning/code.cpp
+++ May/22-Palindrome Partitioning/code.cpp
@@ -1,1 +1,48 @@
 
+class Solution {
+public:
+vector<vector<string>>ans;
+
+bool palindrome(string s)
+{
+    int n=s.size()-1;
+    int i=0;
+    while(i<n)
+    {
+        if(s[i]!=s[n])
+        {
+            return false;
+        }
+        i++;
+        n--;
+    }
+    return true;
+}
+void findAllPalindrome(string s, vector<string> temp)
+{
+    if(s.length()==0)
+    {
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=0;i<s.length();i++)
+        {
+            string firsthalf=s.substr(0,i+1);
+            string secondhalf=s.substr(i+1);
+            if(palindrome(firsthalf))
+            {
+                temp.push_back(firsthalf);
+                findAllPalindrome(secondhalf,temp);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string>temp;
+        findAllPalindrome(s,temp);
+
+        return ans;
+
+        
+    }
+};
