c8c26eab49cc749f3c2cd5be20938a24e6f954b5
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-29 22:31:12 +0530

    Update code.cpp

--- May/29-Number of Steps to Reduce a Number in Binary Representation to One/code.cpp
+++ May/29-Number of Steps to Reduce a Number in Binary Representation to One/code.cpp
@@ -1,1 +1,17 @@
-29-
+class Solution {
+public:
+    int numSteps(string s) {
+        int res=0;
+        int carry=0;
+        for(int i=s.size()-1;i>0;i--)
+        {
+            res++;
+          if(s[i]-'0'+carry==1){
+            carry=1;
+            res++;
+          }
+        }
+        return res+carry;
+        
+    }
+};
