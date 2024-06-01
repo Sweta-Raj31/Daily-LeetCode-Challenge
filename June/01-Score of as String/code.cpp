7fdb260cb24fc33326baaa6a53bb9017e0790819
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-06-01 11:43:16 +0530

    Update code.cpp

--- June/01-Score of as String/code.cpp
+++ June/01-Score of as String/code.cpp
@@ -1,1 +1,14 @@
 
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans=0;
+        for(int i=0;i<s.size()-1;i++)
+        {
+            int val=abs(s[i]-s[i+1]);
+            ans+=val;
+        }
+        return ans;
+        
+    }
+};
