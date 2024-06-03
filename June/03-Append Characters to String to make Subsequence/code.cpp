b2ecd934eabb6c4211421ef8452fc841c47fdcda
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-06-03 22:55:13 +0530

    Update code.cpp

--- June/03-Append Characters to String to make Subsequence/code.cpp
+++ June/03-Append Characters to String to make Subsequence/code.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+       
+       int ns=s.size();
+       int ts=t.size();
+       int i=0;
+       int j=0;
+       for(i=0;i<ns;i++)
+       {
+        if(s[i]==t[j]) j++;
+       }
+       return ts-j;
+        
+    
+    }
+};
