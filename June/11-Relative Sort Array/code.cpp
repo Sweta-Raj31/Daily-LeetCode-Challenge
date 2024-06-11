f9bb50225000c94be49bd3781dae3aff64669020
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-06-11 22:38:53 +0530

    Update code.cpp

--- June/11-Relative Sort Array/code.cpp
+++ June/11-Relative Sort Array/code.cpp
@@ -1,1 +1,24 @@
 
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> ans;
+        unordered_map<int, int>mp;
+        for(int ele : arr1) mp[ele]++;
+        for(int ele : arr2){
+            auto itr = mp.find(ele);
+            ans.insert(ans.end(), itr->second, itr->first);
+            itr->second = 0;
+        }
+        int size = ans.size();
+        
+        for(auto ele : mp){
+            if(ele.second > 0){
+                ans.insert(ans.end(), ele.second, ele.first);
+            }
+        }
+        sort(ans.begin()+size,ans.end());
+
+        return ans;
+    }
+};
