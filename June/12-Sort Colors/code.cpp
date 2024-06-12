b7fadca443640c5c252c2b8c715ad5f6863a9ab3
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-06-12 23:14:49 +0530

    Update code.cpp

--- June/12-Sort Colors/code.cpp
+++ June/12-Sort Colors/code.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0;
+        int start=0;
+        int end=nums.size()-1;
+        while(i<=end)
+        {
+            if(nums[i]==0)
+            {
+                swap(nums[i],nums[start]);
+                i++;
+                start++;
+            }
+            else if(nums[i]==1)
+            {
+                i++;
+            }
+            else
+            {
+                swap(nums[i],nums[end]);
+                 end--;
+            }
+        }
+        
+    }
+};
