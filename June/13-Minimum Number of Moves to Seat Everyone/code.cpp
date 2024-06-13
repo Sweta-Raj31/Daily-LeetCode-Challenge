3b0565e302245e94e335c3e6a8fde4ea1866fae0
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-06-13 22:26:37 +0530

    Update code.cpp

--- June/13-Minimum Number of Moves to Seat Everyone/code.cpp
+++ June/13-Minimum Number of Moves to Seat Everyone/code.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        int ans=0;
+        for(int i=0;i<students.size();i++)
+        {
+            ans=ans+abs(students[i]-seats[i]);
+        }
+        return ans;
+
+        
+    }
+};
