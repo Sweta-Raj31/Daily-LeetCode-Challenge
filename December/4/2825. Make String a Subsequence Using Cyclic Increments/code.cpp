eec0d8570705b9b8d9643051162b34c3b901c5e4
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-12-04 23:40:32 +0530

    Update code.cpp

--- December/4/2825. Make String a Subsequence Using Cyclic Increments/code.cpp
+++ December/4/2825. Make String a Subsequence Using Cyclic Increments/code.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int j = 0;
+        int n = str2.size();
 
+        for(int i = 0; i < str1.size(); i++){
+            char temp = (str1[i] != 'z') ? 1 + str1[i]: 'a';
+            if(str1[i] == str2[j] || temp == str2[j]){
+                j++;
+                if(j == n){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
